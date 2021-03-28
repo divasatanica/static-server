0b68322ef22f462f308ff9ad3ab0e14c0a1fd8fe
divasatanica <jayhebes@vip.qq.com>
2021-03-28 19:48:23 +0800

    feat: 静态路由中间件支持配置资源根目录

--- src/middlewares/static-routes.ts
+++ src/middlewares/static-routes.ts
@@ -3,7 +3,6 @@ import * as path from 'path';
 import { IContext } from '../interfaces/server';
 import { checkMimeTypes } from '../utils/mime-type';
 
-const assetsRoot = path.resolve(__dirname, '../../public');
 const asyncReadFile = (path: string) => new Promise((resolve, reject) => {
   fs.readFile(path, (err, data) => {
     if (err) {
@@ -14,36 +13,42 @@ const asyncReadFile = (path: string) => new Promise((resolve, reject) => {
   })
 })
 
-export async function StaticRoutes(ctx: IContext, next: Function) {
-  const { req } = ctx;
-  const { url } = req;
+export function StaticRoutes(options) {
+  return async function StaticRoutesMiddleware(ctx: IContext, next: Function) {
+    const { req } = ctx;
+    const { url } = req;
+    const { assetsRoot } = options;
 
-  const resourcePath = assetsRoot + (url || '');
-  try {
-    const stat = fs.lstatSync(resourcePath);
-
-    if (!stat.isFile()) {
-      throw new Error();
+    if (!assetsRoot || typeof assetsRoot !== 'string') {
+      throw new Error('AssetsRoot must be set');
     }
-  } catch (e) {
-    ctx.body = 'Not Found'
-    ctx.res.statusCode = 404;
-    await next(ctx);
-    return;
-  }
   
-  try {
-    if (checkMimeTypes(path.extname(url!)) === 'text/html') {
-      ctx.body = fs.createReadStream(resourcePath);
-    } else {
-      const data = await asyncReadFile(resourcePath);
-      ctx.body = data;
+    const resourcePath = assetsRoot + (url || '');
+    try {
+      const stat = fs.lstatSync(resourcePath);
+  
+      if (!stat.isFile()) {
+        throw new Error();
+      }
+    } catch (e) {
+      ctx.body = 'Not Found'
+      ctx.res.statusCode = 404;
+      await next(ctx);
+      return;
     }
-  } catch (e) {
-    ctx.body = JSON.stringify(e);
-    ctx.res.statusCode = 404;
-  } finally {
-    await next(ctx);
+    
+    try {
+      if (checkMimeTypes(path.extname(url!)) === 'text/html') {
+        ctx.body = fs.createReadStream(resourcePath);
+      } else {
+        const data = await asyncReadFile(resourcePath);
+        ctx.body = data;
+      }
+    } catch (e) {
+      ctx.body = JSON.stringify(e);
+      ctx.res.statusCode = 404;
+    } finally {
+      await next(ctx);
+    }
   }
-  
 }
