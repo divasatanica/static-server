77429ce4fe64237a5e7acb955888d0be5ccdf0f7
divasatanica <jayhebes@vip.qq.com>
2021-04-11 10:45:00 +0800

    feat: 增强日志的内容

--- src/middlewares/logger.ts
+++ src/middlewares/logger.ts
@@ -7,12 +7,12 @@ const outSign ='<<<';
 
 export function Logger(logger = console) {
   return async function loggerMiddleware(ctx: IContext, next: Function) {
-    const { req } = ctx;
+    const { req, res } = ctx;
     const { method, url } = req;
     const traceId = `${Date.now()}___${uuid(5)}`;
     const now = Date.now();
     logger.log(sign, inSign, traceId, inSign, new Date().toISOString(), method, url);
     await next(ctx);
-    logger.log(sign, outSign, traceId, outSign, new Date().toISOString(), method, url, 'Time cost:', Date.now() - now, 'ms');
+    logger.log(sign, outSign, traceId, outSign, new Date().toISOString(), method, url, 'StatusCode:', res.statusCode, 'Time cost:', Date.now() - now, 'ms');
   }
 }
