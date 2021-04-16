5eb76d44d9b6a8ee73d4619340603a62752a482b
divastanica <jayhebes@vip.qq.com>
2021-04-16 20:51:01 +0800

    feat: 新增基本权限控制中间件

--- src/middlewares/index.ts
+++ src/middlewares/index.ts
@@ -3,11 +3,13 @@ import { Logger } from './logger';
 import { Timeout } from './timeout';
 import { CacheControl } from './cache-control';
 import { ErrorBoundary } from './error-boundary';
+import { AuthControl } from './auth-control';
 
 export {
   StaticRoutes,
   Logger,
   Timeout,
   CacheControl,
-  ErrorBoundary
+  ErrorBoundary,
+  AuthControl
 }
